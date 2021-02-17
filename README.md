1dc704f8f0ae0acece164cf52818771b7735a3ad
Daniel Collins <daniel.collins11@spsmail.cuny.edu>
2021-02-17 01:44:04 -0500

    IS211 - ASSIGNMENT-2 - DAN COLLINS

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# IS211_Assignment2
+# DAN COLLINS - IS211_Assignment2
 
-Fork this repository if you want
+	- git version 2.30.1.windows.1
+
+	- Python 3.6.5 :: Anaconda, Inc.
+
+# Repo forked from https://github.com/cubanquant/IS211_Assignment2 
+
+# Cloned from https://github.com/dcollins25/IS211_Assignment2
+
+# Professor, 
+
+If you can, please run this program from a Windows command-line. It creates files in the directory it is run in (but not sure if it would work in unix/linux). I imported an additional module [OS] to do this.
+
+Also, I ran the script from Windows and captured the results to the Assignment2-Results.docx Word file. It shows the following:
+	- It shows what happens if you don't supply a URL
+	- It shows what happens if you don't provide a URL with good data.
+	- It shows what happens if you put in something <= 0 (closes the program)
+	- It shows what happens if you put in a good ID # (prints out “Person with ID # is <name> with a birthday of<date>”) - no time
+	- iT shows what happens if you put in a bad ID # - like 999. (prints an error and logs a message to 'errors.log')
+
+# Part V Putting it all Together
+We now need to piece all these functions together. Lets write a main() function that is called when our script runs.
+
+1. First, your program should use the argparse module to allow the user to send a url parameter to the script. This parameter is a string and # is required by the program. If no url is given, the program should exit.
+
+>>> I WAS ABLE TO DO THIS. YOU CAN SEE THIS IN THE FILE AND IF YOU DO NOT PROVIDE A URL A STANDARD MESSAGE WILL SAY IT IS REQUIRED.
+
+2. Then we need to call the downloadData() function and save the results to a variable. Lets call this
+variable csvData. The URL to pass to downloadData() is the URL given as a parameter to the script via argparse. Also, we need to also ensure # that if any exception is thrown by this function, that we catch it, print out an appropriate error message, and exit the program.
+	
+>>> I WAS ABLE TO DO BOTH OF THESE. I CAN DOWNLOAD THE DATA, AND IF YOU PUT IN SOMETHING LIKE http://www.google.com, IT WILL THROW AN ERROR AND LOG IT TO 'errors.log' AS SPECIFIED IN AN 'assignment2' LOGGER.
+
+3. We need to set up a logger with the name ‘assignment2’, which should be configured to write to a single file called “errors.log”. This can # be done in a separate function, or done in the main program.
+
+>>> I WAS ABLE TO DO THIS, AND IT IS DONE VIA A LOGGER CALLED 'assignment2' AND LOGS TO 'errors.log'.
+
+4. Next, we need to take csvData and pass it to the processData() function. The result of this call should be saved into a variable called # personData. Remember, this is a dictionary that maps IDs to tuples.
+
+>>> THIS IS THE ONLY PART THAT I WAS NOT ABLE TO DO. I WAS ABLE TO USE THE DATETIME MODULE TO PUT IT INTO THAT FORMAT, BUT I WAS NOT ABLE TO PUT THE DATA INTO A DICTIONARY. IT CREATED 101 DICTIONARIES INSTEAD OF 1 BIG ONE. I LEFT THE CODE IN THERE SO YOU COULD SEE THE MISTAKE.
+
+>>> GOOD NEWS, I SOLVED AROUND IT! I INTRODUCED A NEW MODULE AND SENT THE DATA TO A TEXT FILE. LATER, I READ IN THAT TEXT FILE AND SEARCH IT FOR THE PERSON, BIRTHDATE BASED ON THE USERS <STDIN> WITH ID #. I SPLIT A BUNCH OF TIMES AND MASSAGE THE OUTPUT TO GET SOMETHING CLOSE TO WHAT YOU REQUESTED.
+	- ALSO, IF YOU PUT IN THE WRONG ID # (like 999) IT WILL PRINT AN ERROR MESSAGE AND LOG IT TO 'errors.log'
+
--- assignment2.py
+++ assignment2.py
@@ -1,26 +1,142 @@
 import argparse
 import urllib.request
 import logging
-import datetime
+from datetime import datetime
+import os
+
+# Check to see if these files below exist, remove them if they do. To be used later ...
+if os.path.exists("IS211-ext-file.txt"):
+    os.remove("IS211-ext-file.txt")
+if os.path.exists("errors.log"):
+    os.remove("errors.log")
+
+LOG_FILENAME = 'errors.log'
+logging.basicConfig(
+    filename=LOG_FILENAME,
+    level=logging.DEBUG,
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("URL")
+link = parser.parse_args()
+#print(link.URL)
+strurl = (link.URL)
 
 def downloadData(url):
-    """Downloads the data"""
-    pass
+    try:
+        req = urllib.request.urlopen(url)
+        decoded = str(req.read().decode('utf-8'))
+    except Exception as Argument:
+        assignment2 = open(LOG_FILENAME, "a")
+        assignment2.write(str(Argument))
+        assignment2.close()
+        print('\n*** ' + 'An ERROR occurred. Please see \"errors.log\" for more information ***')
+        exit()
+    return decoded
+
+#content = downloadData('https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv')
+csvData = downloadData(strurl)
 
 def processData(file_content):
-    pass
+    myList = file_content.split('\n')
+    userdict = {}
+    counter = 0
+    newDate = ''
+    for line in myList:
+        counter +=1
+        userTuple = line.split(',')
+        if len(userTuple) == 3:
+            number, person, date = (userTuple)
+# PUT LOGGER BELOW
+            try:
+                newDate = datetime.strptime(date, '%d/%m/%Y')
+                #print(type(newDate))
+                #print(number, person, newDate)
+                newTuple = (number, person, newDate)
 
+            except:
+                pass
 
-def displayPerson(id, personData):
-    pass
+        userdict = {
+            (counter): ((person), (newDate))
+                    }
+        #print(userdict)
+# I COULD NOT GET THE DATA INTO ONE BIG DICTIONARY. IT CREATED 102 INDIVIDUAL DICTIONARIES, SO I DUMPED THE DATA INTO A FILE.
+# WHEN THE USER INPUTS THE ID TO SEARCH, I SEARCH THROUGH THE FILE "IS211-ext-file.txt" AND PRESENT THE DATA RETRIEVED FROM THAT.
+        userstr = str(userdict)
+        outF = open("IS211-ext-file.txt", "a")
+        outF.write(userstr)
+        outF.write("\n")
+        outF.close()
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+noUser = 0
 
+processData(csvData)
 
-if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
+#def displayPerson(id, personData):
+#    pass
+#
+# TAKE STANDARD IN FROM USER AND ASK FOR AN ID
+#	- if ID <= 0, end the program
+#	- if ID is in file, display the user and date
+#	- if ID is not in file, print 'No user # found with that ID'
+
+inputID = input("Please enter the ID of the user you need information on: ") 
+
+integerID = int(inputID)
+
+if integerID <= 0:
+    print("Goodbye!")
+    exit()
+
+if integerID == 1:
+    print("Line 1 is simply a Header. Please choose another value.")
+    exit()
+
+searchChars = "{" + inputID + ": "
+#print("SEARCHING FOR: " + searchChars)
+
+searchfile = open("IS211-ext-file.txt", "r")
+for line in searchfile:
+    if searchChars in line: 
+        #print(line)
+        splitOne = line.split('datetime.datetime(')
+        splitOneStr = str(splitOne)
+        splitTwo = splitOneStr.split(',')
+        splitTwoStr = str(splitTwo[0] + splitTwo[2] + splitTwo[3] + splitTwo[4])
+        #print(splitTwoStr)
+# SAMPLE DATA: ["{91: ('Sonia Pullman' '1999 9 14
+        splitThree = splitTwoStr.split(': (')
+        splitThreeStr = str(splitThree[1])
+        #print(splitThreeStr)
+# SAMPLE DATA: 'Sonia Pullman' '1999 9 14
+        splitFour = splitThreeStr.split()
+        #print(splitFour)
+# SAMPLE DATA: ["'Sonia", "Pullman'", "'1999", '9', '14']
+
+        if len(splitFour[3]) == 1:
+            paddedIndexThree = "0" + splitFour[3]
+            newIndexThree = paddedIndexThree
+        else:
+            newIndexThree = splitFour[3]
+
+        if len(splitFour[4]) == 1:
+            paddedIndexFour = "0" + splitFour[4]
+            newIndexFour = paddedIndexFour
+        else:
+            newIndexFour = splitFour[4]
+
+        print('\n' + 'Person with # ' + (inputID) + ' is ' + (splitFour[0]) + ' ' + (splitFour[1]) + ' with a birthday of ' + (splitFour[2]) + ' ' + (newIndexThree) + ' ' + (newIndexFour))
+        exit()
+
+    else:
+        tempF = open(LOG_FILENAME, "a")
+        tempF.write('No user # found with that ID')
+        tempF.write("\n")
+        tempF.close()
+        noUser = 1
+
+if noUser == 1:
+    print('\n*** ' + 'An ERROR occurred searching for this user ID #. Please see \"errors.log\" for more information ***')
+
+searchfile.close()
